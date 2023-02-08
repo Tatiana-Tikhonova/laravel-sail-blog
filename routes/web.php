a15cdd68ce8fb47cde4bc471167dbf798607560d
Tatiana-Tikhonova <tantikh2020@gmail.com>
2023-02-08 13:04:23 +0300

    add static templates and router constraints

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
-Route::get('post', function () {
-    return view('post');
-});
+Route::get('posts/{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    if(!file_exists($path)) return redirect('/');
+
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(10), fn() => file_get_contents($path););
+    
+    return view('post',[ 'post'=> $post ]);
+    
+})->where('post','[A-z_\-]+');
+
+    
